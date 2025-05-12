e8af33d3310f4fcbcceb54d71e3a179a4a63a0d2
Yanis101K <yaniskaced41@gmail.com>
2025-05-12 11:08:45 +0100

    project setup

---
+++ project-setup.sh
@@ -0,0 +1,4 @@
+touch app.py requirements.txt .gitignore README.md &&
+mkdir -p static/styles static/scripts static/images templates/admin instance/ && 
+touch templates/base.html templates/home.html templates/about.html templates/projects.html templates/contacts.html templates/admin/login.html templates/admin/dashboard.html instance/config.py
+mkdir database && touch database/portfolio.db 
